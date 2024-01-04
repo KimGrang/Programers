44ef6ed906d3a60dd1f70ba17110bcc16fc41b41
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-04 12:07:40 +0900

    Update 정수 제곱근 판별.js

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,12 +1,8 @@
 function solution(n) {
-    var answer = 0;
-
     for(let i=0, a=0; i<=n; i++){
-      if(n/i===i){
-        a = i + 1
-        return a*a
-      } else
-        answer = -1;
+        if(n/i === i){
+            return (i+1)*(i+1)
+        }
     }
-    return answer;
+    return -1
 }
