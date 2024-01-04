cb685c3baa467d7f3fb0264fc482860278270d10
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-04 11:06:15 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,9 +1,4 @@
 function solution(arr) {
     var answer = 0;
-    let len = arr.length 
-    for(let i=0;i<len;i++){
-        answer += Number(arr[i])
-    }
-    answer /=len
     return answer;
 }
