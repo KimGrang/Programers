08bb30ba04f1c8ce94e48634c6ede5abcc393d28
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-05 11:16:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    var answer = 0;
+    while (num!=1 && answer !=500){
+        if(num%2===0){
+            num=num/2
+        }
+        else{
+            num=num*3+1
+        }
+        answer+=1;
+    }
+    if(num==1){
+        return answer;
+    }
+    else{
+        return -1;
+    }
+}
