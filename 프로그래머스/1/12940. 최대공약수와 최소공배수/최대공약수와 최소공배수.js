7e1db91165f2740a25a991de7b0568fe405317a6
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-09 10:02:27 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,7 @@
+function solution(n, m) {
+    var answer = [];
+    const gcd = (n,m)=>(n%m)===0? m:gcd(m,(n%m));
+    const lcm = (n,m)=>n*m/gcd(n,m);
+    answer.push(gcd(n,m),lcm(n,m))
+    return answer;
+}
