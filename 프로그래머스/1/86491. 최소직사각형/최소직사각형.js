7ccff6dfb156d45cd65b3e0e3a64b1d94beebb9b
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-10 09:32:13 +0900

    [level 1] Title: 최소직사각형, Time: 488.96 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,19 @@
+function solution(sizes) {
+    let answer = 0;
+    let big = []
+    let small = []
+    for(let i=0;i<sizes.length;i++){
+        if(sizes[i][0]>sizes[i][1]){
+            big.push(sizes[i][0])
+            small.push(sizes[i][1])
+        }
+        else{
+            small.push(sizes[i][0])
+            big.push(sizes[i][1])
+        }
+    answer = Math.max(...big)*Math.max(...small)    
+    }
+    return answer
+}
+
+    
