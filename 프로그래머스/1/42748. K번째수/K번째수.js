a2d81be9866c2268ee7b1ee5fb30714d74e1533c
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-11 09:51:42 +0900

    Update K번째수.js

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -5,6 +5,9 @@ function solution(array, commands) {
         let end = commands[i][1]
         let idx = commands[i][2]-1
         answer.push(array.slice(start,end).sort((a,b)=>a-b)[idx])
+        // "sort()는 compareFunction이 제공되지 않으면 유니코드 순서로 문자열을 비교해서 정렬합니다."
+        // 라는 글을 읽고 sort에 compareFunction 추가
     }
     return answer;
 }
+
