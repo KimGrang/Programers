569ed7152f245f3afb74a43d8412f4ce5c3f3b15
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-11 09:48:26 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    var answer = [];
+    for(let i=0;i<commands.length;i++){
+        let start = commands[i][0]-1
+        let end = commands[i][1]
+        let idx = commands[i][2]-1
+        answer.push(array.slice(start,end).sort((a,b)=>a-b)[idx])
+    }
+    return answer;
+}
