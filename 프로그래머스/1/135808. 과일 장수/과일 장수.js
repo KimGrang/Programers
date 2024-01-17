e66ab10c79e047a492b31f3825588e095fbb8710
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-01-17 09:39:40 +0900

    [level 1] Title: 과일 장수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,7 +1,7 @@
 function solution(k, m, score) {
     if(score.length < m) return 0
     var answer = 0;
-    score.sort((a, b) => a - b);
+    score.sort();
     while(score.length >= m){
         let box = score.splice(score.length-m,m);
         answer += box[0]*m  
