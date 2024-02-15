ae4bd844760617ec58d5dae3670356a9a68828e5
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-02-15 09:57:36 +0900

    Update 둘만의 암호.js

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -2,8 +2,6 @@ function solution(s, skip, index) {
     let answer = ''
     const alphabet = 
           'abcdefghijklmnopqrstuvwxyz'.split('').filter((n)=>!skip.includes(n))
-    let _s = s.split('')
-    
     answer = s.split('').map((a) => alphabet[(alphabet.indexOf(a)+index) % alphabet.length]).join('');
 
     return answer
