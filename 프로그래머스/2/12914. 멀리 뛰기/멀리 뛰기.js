618e3d5d5876514bdceca6214cad48d0320788d7
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-03-14 13:19:21 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    const dp = [0,1,2]
+    const answer = [0,1,2]
   	for(let i=3;i<=n;i++){
-      dp[i]=(dp[i-1]+dp[i-2])%1234567
+      answer[i]=(answer[i-1]+answer[i-2])%1234567
     }
-  return dp[n];
+  return answer[n];
 }
-
-
